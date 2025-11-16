9f27202ff77b1561b6374aafde42b551e46cfa42
Toby Liu <ybot1122@gmail.com>
2025-11-15 17:50:39 -0800

    fix ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://stfu-and-eat.vercel.app/",
   integrations: [sitemap(), mdx(), pagefind()],
   vite: {
+    resolve: {
+      alias: {
+        // allow imports like '@components/...' or '@/components/...'
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
     plugins: [tailwindcss()],
   },
   markdown: {
